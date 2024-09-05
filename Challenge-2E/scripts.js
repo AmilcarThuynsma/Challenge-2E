b1969cfbd755c6757d056c641512f7e33e9a86db
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-09-05 09:34:57 +0200

    fix alex.js

--- Challenge-2E/scripts.js
+++ Challenge-2E/scripts.js
@@ -2,4 +2,4 @@ import * as nwabisa from './nwabisa';
 import * as johannes from './johannes';
 import * as alex from './alex';
 
-console.log('Roles:', nwabisa.role, johannes.role, alex.role)
+console.log('Roles:', nwabisa.role, johannes.role, alex.role);
